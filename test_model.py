f5b8c5ec00fd88b69251b2350146683294cb7c9b
Charlie New <charlieniu21@gmail.com>
2019-05-12 22:00:53 +0800

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -900,7 +900,7 @@ def create_generator_loss(disc_output, gene_output, gene_output_complex,  gene_o
     feature_mask = masks 
     dc_loss_list = []
     for i in range(30):
-        gene_kspace = Fourier(gene_output_list[i] separate_complex=True)
+        gene_kspace = Fourier(gene_output_list[i], separate_complex=True)
         loss_kspace = tf.cast(tf.abs(tf.square(gene_kspace - feature_kspace)),tf.float32)*tf.cast(feature_mask,tf.float32)
         dc_loss  = tf.reduce_mean(loss_kspace)
         dc_loss_list.append(dc_loss)
