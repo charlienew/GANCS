85d1f0ab5b7f313e84d18e98ad6a21558d7cf341
Charlie New <charlieniu21@gmail.com>
2019-05-14 00:49:37 +0800

    Update ori_model.py

--- ori_model.py
+++ ori_model.py
@@ -852,7 +852,7 @@ def keras_var(x, axis=None, keepdims=False):
     """
     # axis = _normalize_axis(axis, ndim(x))
     if x.dtype.base_dtype == tf.bool:
-        x = tf.cast(x, floatx())
+        x = tf.cast(x, tf.float32)
     m = tf.reduce_mean(x, reduction_indices=axis, keep_dims=True)
     devs_squared = tf.square(x - m)
     return tf.reduce_mean(devs_squared,
